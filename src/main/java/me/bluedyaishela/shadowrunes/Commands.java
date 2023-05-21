5e6b266dcef37ff85e03d13f5b567f933fb568a8
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-21 19:34:25 +0200

    #9 Ajout des commandes permettant de se donner les armes et runes de dégâts

--- src/main/java/me/bluedyaishela/shadowrunes/Commands.java
+++ src/main/java/me/bluedyaishela/shadowrunes/Commands.java
@@ -5,6 +5,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -12,6 +14,20 @@ public class Commands implements CommandExecutor {
         if(sender instanceof Player)
         {
             Player player = (Player)sender;
+            if (args.length == 0)
+            {
+                player.sendMessage("§cVous n'avez pas sélectionné d'arguments lors de l'exécution de la commande, ré-essayez.");
+                player.sendMessage("§cExemple : /shadowrunes <value>");
+            } else {
+                String argument = args[0];
+
+                if (argument.equals("damagesword"))
+                {
+                    player.getInventory().addItem(ItemManager.DamageSword);
+                } else if (argument.equals("damagerune")) {
+                    player.getInventory().addItem(ItemManager.DamageRunes);
+                }
+            }
             player.sendMessage("§cCommande réussie.");
         }
         return false;
