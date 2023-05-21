c59de5fbfc4f2568810a1f62f9b7e144f9984d36
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-21 03:43:50 +0200

    #2 Initialisation des fichiers nécessaires pour l'ajout des items

---
+++ src/main/java/me/bluedyaishela/shadowrunes/ItemManager.java
@@ -0,0 +1,21 @@
+package me.bluedyaishela.shadowrunes;
+
+import org.bukkit.inventory.ItemStack;
+
+public class ItemManager {
+
+    public static ItemStack DamageSword;
+    public static ItemStack DamageRunes;
+
+    public static void init() {
+        createDamageSword();
+        createDamageRunes();
+    }
+
+    private static void createDamageSword() {
+        // Crée l'épée qui possède des dégâts supplémentaires (lore)
+    }
+    private static void createDamageRunes() {
+        // Crée une rune pouvant être ajoutée sur une épée
+    }
+}
---
+++ src/main/java/me/bluedyaishela/shadowrunes/runes/Damage.java
@@ -0,0 +1,16 @@
+package me.bluedyaishela.shadowrunes.runes;
+
+import me.bluedyaishela.shadowrunes.ShadowRunes;
+import org.bukkit.event.Listener;
+
+public class Damage implements Listener {
+
+    ShadowRunes plugin;
+
+    public Damage(ShadowRunes plugin)
+    {
+        this.plugin = plugin;
+    }
+
+
+}
