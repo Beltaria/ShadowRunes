9ad47863ccf67846cd9a6a77139770e0e6b89e73
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-21 02:48:10 +0200

    #7 Ajout du fichier de configuration et création du fichier de configuration au démarrage du serveur.

--- src/main/java/me/bluedyaishela/shadowrunes/ShadowRunes.java
+++ src/main/java/me/bluedyaishela/shadowrunes/ShadowRunes.java
@@ -1,17 +1,38 @@
 package me.bluedyaishela.shadowrunes;
 
+import com.sun.org.apache.bcel.internal.generic.DMUL;
+import me.bluedyaishela.shadowrunes.runes.Damage;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class ShadowRunes extends JavaPlugin {
+
+    public FileConfiguration config;
+    public File cfile;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.cfile = new File(getDataFolder(), "config.yml");
+        if (!this.cfile.exists())
+        {
+            this.getLogger().info("Le fichier config.yml n'a pas été trouvé, création en cours...");
+            this.saveDefaultConfig();
+        } else {
+            this.getLogger().info("Le fichier de configuration a été trouvé, chargement en cours...");
+        }
+        this.config = this.getConfig();
 
+//        this.getServer().getPluginManager().registerEvents(new Damage(this), this);
+
+        System.out.println("FoodEffect a démarré avec succès.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("FoodEffect s'est arrêté avec succès.");
     }
 }
