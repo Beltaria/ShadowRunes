51df8b2736fa1e05bbc43f574c6dff9024d11683
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-22 05:13:20 +0200

    #2 Ajout de la rune de dégâts sur l'arme Note : Ne fonctionne pas correctement, il faut glisser en premier la hache sur l'étoile or ça devrait être l'inverse + s'il ne reste plus qu'une seule étoile, celle-ci n'est pas supprimée.

--- src/main/java/me/bluedyaishela/shadowrunes/runes/Damage.java
+++ src/main/java/me/bluedyaishela/shadowrunes/runes/Damage.java
@@ -33,16 +33,38 @@ public class Damage implements Listener {
             return;
         }
 
+        // On récupère le joueur qui a cliqué et l'objet actuellement sélectionné
         Player player = (Player) event.getWhoClicked();
         ItemStack clickedItem = event.getCurrentItem();
 
-        if (clickedItem != null && clickedItem.getType() == Material.DIAMOND_AXE) {
-            ItemMeta clickedMeta = clickedItem.getItemMeta();
-
-            List<String> lore = new ArrayList<>();
-            lore.add("Bonus");
-            clickedMeta.setLore(lore);
-            clickedItem.setItemMeta(clickedMeta);
+        System.out.println("Stade 1 : " + clickedItem);
+        // On vérifie que l'item est bien une étoile du nether
+        if (clickedItem != null && clickedItem.getType() == Material.NETHER_STAR)
+        {
+            // On vérifie ensuite que le lore est valide
+            ItemMeta itemMeta = clickedItem.getItemMeta();
+            System.out.println("Stade 2 : " + itemMeta);
+            if (itemMeta != null && itemMeta.getLore() != null && itemMeta.getLore().contains("damage"))
+            {
+                // On récupère ensuite l'objet dans l'inventaire au dépôt de l'étoile et on vérifie que ce n'est pas vide.
+                ItemStack weapon = player.getItemOnCursor();
+                System.out.println("Stade 3 : " + weapon);
+                if (weapon != null && weapon.getType() == Material.DIAMOND_AXE)
+                {
+                    ItemMeta weaponMeta = weapon.getItemMeta();
+                    System.out.println("Stade 4 : " + weaponMeta);
+                    if (weaponMeta != null) {
+                        weaponMeta.setLore(Collections.singletonList("bonus damage"));
+                        weapon.setItemMeta(weaponMeta);
+                        if (clickedItem.getAmount() > 1) {
+                            clickedItem.setAmount(clickedItem.getAmount() - 1);
+                        } else {
+                            clickedItem.setAmount(0);
+//                            player.getInventory().remove(clickedItem);
+                        }
+                    }
+                }
+            }
         }
 
     }
