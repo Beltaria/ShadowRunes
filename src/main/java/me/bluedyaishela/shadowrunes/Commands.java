76139abd77033b04475dc5f075fc5a89c98df580
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-22 02:17:36 +0200

    #9 Modification des commandes avec l'ajout du prefix

--- src/main/java/me/bluedyaishela/shadowrunes/Commands.java
+++ src/main/java/me/bluedyaishela/shadowrunes/Commands.java
@@ -3,32 +3,43 @@ package me.bluedyaishela.shadowrunes;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-import java.util.Objects;
-
 public class Commands implements CommandExecutor {
+
+    private final ShadowRunes main;
+    public Commands(ShadowRunes plugin) {
+        this.main = plugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        FileConfiguration config = main.getConfig();
 
         if(sender instanceof Player)
         {
             Player player = (Player)sender;
             if (args.length == 0)
             {
-                player.sendMessage("§cVous n'avez pas sélectionné d'arguments lors de l'exécution de la commande, ré-essayez.");
-                player.sendMessage("§cExemple : /shadowrunes <value>");
+                player.sendMessage(config.getString("plugin_commands") + "§cVous n'avez pas sélectionné d'arguments lors de l'exécution de la commande, ré-essayez.");
+                player.sendMessage(config.getString("plugin_commands") + "§cExemple : /shadowrunes <value>");
             } else {
                 String argument = args[0];
 
                 if (argument.equals("damagesword"))
                 {
                     player.getInventory().addItem(ItemManager.DamageSword);
-                } else if (argument.equals("damagerune")) {
+                    player.sendMessage(config.getString("plugin_commands") + "§cVous avez obtenue l'arme de dégâts.");
+                }
+                else if (argument.equals("damagerune"))
+                {
                     player.getInventory().addItem(ItemManager.DamageRunes);
+                    player.sendMessage(config.getString("plugin_commands") + "§cCommande réussie.");
                 }
             }
-            player.sendMessage("§cCommande réussie.");
+            player.sendMessage(config.getString("plugin_commands") + "§cCommande réussie.");
         }
         return false;
     }
